b44aa403a16ff0d041d14261e6946918454e4f89
EQuimper <quimperemanuel@gmail.com>
2016-04-13 16:13:24 -0600

    Init Commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Component } from 'react';
 
+import SearchBar from '../containers/search_bar';
+
 export default class App extends Component {
   render() {
     return (
