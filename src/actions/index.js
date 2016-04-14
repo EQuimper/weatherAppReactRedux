e7b7d192512b78b48398824ad09109ec63ddff99
EQuimper <quimperemanuel@gmail.com>
2016-04-13 21:27:56 -0600

    Add axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 const API_KEY = '70f9936bb16d2c22a5aa5462bf6ff682';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},ca`;
+	
+	return {
+		type: FETCH_WEATHER
+	};
+}
